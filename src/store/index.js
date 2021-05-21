0713712b8faac9e51d7dd66d68e97177cffea225
tlccstudio <77962211+tlccstudio@users.noreply.github.com>
2021-05-21 17:53:03 +0700

    Add files via upload

--- src/store/index.js
+++ src/store/index.js
@@ -3,14 +3,19 @@ import Vuex from "vuex";
 
 import account from "./account";
 import general from "./general";
+import oracles from "./oracles";
 
 Vue.use(Vuex);
 
 export default function() {
   const Store = new Vuex.Store({
+    state: {
+      text: "STATE DATA TEST -- AAB5"
+    },
     modules: {
       general,
-      account
+      account,
+      oracles
     },
 
     strict: process.env.DEV
